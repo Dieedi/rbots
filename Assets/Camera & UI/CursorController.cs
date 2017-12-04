85e507ad5f00d1900253c0ae3ef44da74f7a96c2
Jeremy 'Dieedi' Pouillot <pouillot.j@free.fr>
2017-12-04 09:26:56 +0100

    adding cursors & cursor changes, navmeshsurfaces depending on navmeshagents

---
+++ Assets/Camera & UI/CursorController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+	[SerializeField] Texture2D walkCursor = null;
+	[SerializeField] Texture2D combatCursor = null;
+	[SerializeField] Texture2D otherCursor = null;
+	[SerializeField] Vector2 cursorHotspot = new Vector2(0, 0);
+
+	private CameraRaycaster raycaster;
+
+	// Use this for initialization
+	void Start()
+	{
+		raycaster = GetComponent<CameraRaycaster>();
+		//Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+		raycaster.O_LayerChange += OnLayerChange;
+	}
+
+	void OnLayerChange(Layer layerHit)
+	{
+		switch (layerHit) {
+			case Layer.Enemy:
+				Cursor.SetCursor(combatCursor, cursorHotspot, CursorMode.Auto);
+				break;
+			case Layer.Walkable:
+				Cursor.SetCursor(null, cursorHotspot, CursorMode.Auto);
+				break;
+			default:
+				//Cursor.SetCursor(otherCursor, cursorHotspot, CursorMode.Auto);
+				return;
+		}		
+	}
+}
